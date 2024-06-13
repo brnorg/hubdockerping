bac7dbdd8bdc2367e7580e5ebbe77339b9e9b38d
Bruno Souza <56989493+brunosoouza@users.noreply.github.com>
2024-06-13 13:14:12 -0300

    Update latest.sh

--- latest.sh
+++ latest.sh
@@ -4,14 +4,6 @@
 URL='https://hub.docker.com/v2/repositories/pingidentity/pingfederate/tags?page_size=25&page=1&ordering=&name=latest'
 
 # Obtendo os dados JSON usando o curl e filtrando a última versão que segue o padrão
-latest_versions=()
-while IFS= read -r line; do
-    if [[ $line =~ [0-9]+\.[0-9]+\.[0-9]+-latest ]]; then
-        latest_versions+=("$line")
-    fi
-done < <(curl -s "$URL" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+-latest')
-
-# Ordenar as versões e pegar a última
-latest_version=$(printf "%s\n" "${latest_versions[@]}" | sort -V | tail -n 1)
+latest_version=$(curl -s "$URL" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+-latest' | sort -V | tail -n 1)
 
 echo "A última versão com o padrão 'x.x.x-latest' é: $latest_version"
